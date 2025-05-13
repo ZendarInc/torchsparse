452872d89b265b46e5e80c0794ba0f532a14c9da
Arnaud Louys <arnaud@zendar.io>
2025-05-13 11:45:15 +0200

    Add fixes from pre-commit

--- setup.py
+++ setup.py
@@ -11,9 +11,8 @@ from torchsparseplusplus import __version__
 
 version = __version__
 
-if (torch.cuda.is_available() and CUDA_HOME is not None) or (
-    os.getenv('FORCE_CUDA', '0') == '1'
-):
+if (torch.cuda.is_available() and CUDA_HOME is not None) or (os.getenv(
+        'FORCE_CUDA', '0') == '1'):
     device = 'cuda'
     pybind_fn = f"pybind_{device}.cu"
 else:
@@ -21,19 +20,21 @@ else:
     pybind_fn = f"pybind_{device}.cpp"
 
 sources = [os.path.join('torchsparseplusplus', 'backend', pybind_fn)]
-for fpath in glob.glob(os.path.join('torchsparseplusplus', 'backend', '**', '*')):
-    if (fpath.endswith('_cpu.cpp') and device in ['cpu', 'cuda']) or (
-        fpath.endswith('_cuda.cu') and device == 'cuda'
-    ):
+for fpath in glob.glob(os.path.join('torchsparseplusplus', 'backend', '**',
+                                    '*')):
+    if (fpath.endswith('_cpu.cpp')
+            and device in ['cpu', 'cuda']) or (fpath.endswith('_cuda.cu')
+                                               and device == 'cuda'):
         sources.append(fpath)
 
 extension_type = CUDAExtension if device == 'cuda' else CppExtension
 extra_compile_args = {
     'cxx': ['-g', '-O3', '-fopenmp', '-lgomp'],
-    'nvcc': ['-O3', '-std=c++17',
-             '-gencode=arch=compute_80,code=sm_80',
-             '-gencode=arch=compute_86,code=sm_86',
-             '-gencode=arch=compute_89,code=sm_89'],
+    'nvcc': [
+        '-O3', '-std=c++17', '-gencode=arch=compute_80,code=sm_80',
+        '-gencode=arch=compute_86,code=sm_86',
+        '-gencode=arch=compute_89,code=sm_89'
+    ],
 }
 
 setup(
@@ -41,9 +42,9 @@ setup(
     version=version,
     packages=find_packages(),
     ext_modules=[
-        extension_type(
-            'torchsparseplusplus.backend', sources, extra_compile_args=extra_compile_args
-        )
+        extension_type('torchsparseplusplus.backend',
+                       sources,
+                       extra_compile_args=extra_compile_args)
     ],
     install_requires=[
         'numpy',
